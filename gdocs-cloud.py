9926b4234cbc988fe709bf8f84efc7e6c6c62223
Alex Yule <alexy@codeforamerica.org>
2012-02-29 02:11:53 -0500

    outputs final string file. everything else pretty much working

--- gdocs-cloud.py
+++ gdocs-cloud.py
@@ -3,7 +3,8 @@ import gdata.docs.client
 import gdata.auth
 import os
 import redis
-import re
+import lxml.html
+
 
 r = redis.StrictRedis()
 
@@ -43,18 +44,43 @@ def ProcessFeed(feed):
       if r.sadd("filenames", filename):
         client.download_resource(entry, filename) # is this synchronous?
 
+# http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
+# class MLStripper(HTMLParser):
+#     def __init__(self):
+#         self.reset()
+#         self.fed = []
+#     def handle_data(self, d):
+#         self.fed.append(d)
+#     def get_data(self):
+#         return ' '.join(self.fed)
+
+def strip_tags(html):
+  string = ''
+  html = lxml.html.fromstring(html)
+  string += html.cssselect('title')[0].text_content() + ' '
+  string += html.cssselect('body')[0].text_content() + ' '
+  return string
+    # s = MLStripper()
+    # s.feed(html)
+    # return s.get_data()
+
 def ScrapeFiles():
+  
   for filename in r.smembers("filenames"):
     if filename not in r.smembers("scraped_files"):
-      try:
-        f = open(filename, 'r').read()
-        r.sadd("scraped_text", f)
-        r.sadd("scraped_files", filename)
-        f.close()
-      except:
-        print 'error scraping file: '+filename
-  print r.smembers("scraped_files")
-  print r.smembers("scraped_text")
+      print 'scraping ' + filename
+      f = open(filename, 'r')
+      text = strip_tags(f.read())
+      
+      r.sadd("scraped_text", text)
+      r.sadd("scraped_files", filename)
+      f.close()
+  
+  output_file = open(files + 'output.txt', 'a')
+  for string in r.smembers("scraped_text"): output_file.write(string)
+  output_file.close()
+
 
 ProcessFeed('https://docs.google.com/feeds/default/private/full/folder%3A0B_k36WQYssQgYzU2NTcwNDYtZWZmOC00NTY2LWI2MzAtNTEwZDE0ZmJkNTVj/contents')
 ScrapeFiles()
+
